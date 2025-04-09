e6f60d8b7877ef6854c8965fbd5389c7c14745d8
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-04-09 16:56:16 +0600

    Add Vehicle and TravelAgent pages with component imports and data mapping

---
+++ src/app/services/(rent)/vehicles/page.jsx
@@ -0,0 +1,23 @@
+import VehicleCard from "@/components/allCard/VehicleCard";
+import vehiclesData from "@/utils/vehiclesData";
+
+
+
+
+const page = () => {
+
+
+    return (
+      <div className='w-11/12 mx-auto md:w-10/12'>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10 py-10">
+          {vehiclesData.map((item, index) => (
+            <div key={item.id}>
+              <VehicleCard item={item} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default page
