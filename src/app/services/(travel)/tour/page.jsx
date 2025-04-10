921137561938ef4969a667ee6e8284e792f928ef
shakil-ahmed-billal <itzxhakil@gmail.com>
2025-04-10 14:27:43 +0600

    Refactor import statements for consistency in Tour and Travel Agent pages

--- src/app/services/(travel)/tour/page.jsx
+++ src/app/services/(travel)/tour/page.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
-
-import tourPackages from '@/utils/tourPackageData';
 import TourPackageCard from '@/components/allCard/TourPackageCard';
+import tourPackages from '@/utils/tourPackageData';
+
 
 
 const page = () => {
