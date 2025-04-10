97a08b93156ad3be4e6e43feb626232e86fcfb28
saad-12-3 <mosayebhossain.o@gmail.com>
2025-04-10 17:13:02 +0600

    Add Facebook Ads page and data implement FacebookAdsCard component

--- src/app/services/(buyAndSellServics)/powerBank/page.jsx
+++ src/app/services/(buyAndSellServics)/powerBank/page.jsx
@@ -5,7 +5,7 @@ import powerBanksData from "@/utils/powerBanksData";
 const Page = () => {
   return (
     <div className='w-11/12 mx-auto md:w-10/12'>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-10">
         {powerBanksData.map((powerBank) => (
           <div key={powerBank.product_id}>
             <PowerBankCard powerBank={powerBank} />
